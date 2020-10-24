5a0b0af62f7fc1efb34f6949598f5afc29c0ddaa
Gabriele Fassina <gfassina.97@gmail.com>
2020-10-24 19:22:51 +0200

    Interrupt header

---
+++ FASSINA_GABRIELE.cydsn/InterruptRoutines.h
@@ -0,0 +1,21 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#ifndef _INTERRUPT_ROUTINES_H_
+    // Header guard
+    #define _INTERRUPT_ROUTINES_H_
+    
+    #include "project.h"
+    
+    CY_ISR_PROTO(Custom_RX_ISR);
+    
+#endif
+/* [] END OF FILE */
