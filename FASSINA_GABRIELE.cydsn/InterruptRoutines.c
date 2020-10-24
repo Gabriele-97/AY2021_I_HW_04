7e733b145f2972c336987a6505c4b1750ba3d05f
Gabriele Fassina <gfassina.97@gmail.com>
2020-10-24 19:39:51 +0200

    implemented ADC reading

--- FASSINA_GABRIELE.cydsn/InterruptRoutines.c
+++ FASSINA_GABRIELE.cydsn/InterruptRoutines.c
@@ -10,6 +10,9 @@
  * ========================================
 */
 #include "InterruptRoutines.h"
+int32_t value_pot;
+int32_t value_photo;
+uint k=0;
 
 CY_ISR(Custom_RX_ISR){
     
@@ -17,6 +20,14 @@ CY_ISR(Custom_RX_ISR){
 
 
 CY_ISR(Custom_ADC_ISR){
-
+    Timer_ReadStatusRegister();
+    AMux_FastSelect(POTENTIOMETER_CHANNEL);
+    value_pot = ADC_DelSig_Read32();
+    k++;
+    if(k == 10){ //il sampling del photoresistor viene fatto con una frequenza più bassa del potenziometro
+    AMux_FastSelect(PHOTORESISTOR_CHANNEL);
+    value_photo = ADC_DelSig_Read32();
+    k=0;
+    }
 }
 /* [] END OF FILE */
--- FASSINA_GABRIELE.cydsn/InterruptRoutines.h
+++ FASSINA_GABRIELE.cydsn/InterruptRoutines.h
@@ -14,6 +14,15 @@
     #define _INTERRUPT_ROUTINES_H_
     
     #include "project.h"
+    #include "cytypes.h"
+    #include "stdio.h"
+    
+    #define TRANSMIT_BUFFER_SIZE 16
+    #define POTENTIOMETER_CHANNEL 0
+    #define PHOTORESISTOR_CHANNEL 1
+    
+    char DataBuffer[TRANSMIT_BUFFER_SIZE];
+    volatile uint8 PacketReadyFlag;
     
     CY_ISR_PROTO(Custom_RX_ISR);
     CY_ISR_PROTO(Custom_ADC_ISR);
